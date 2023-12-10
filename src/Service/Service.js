085b663a919426580307ee39e71e131d7cf4de55
Jagadeesh <jagadeesh.vadranapu@gmail.com>
2023-12-10 21:56:03 +0530

    service changes

--- src/Service/Service.js
+++ src/Service/Service.js
@@ -1,16 +1,19 @@
-const ApiURL = "https://jagadeeshwebapplication1api.azure-api.net/api";
+const ApiURL = "https://jagadeeshwebapplication1api.azure-api.net/api"; //"https://localhost:7249/api";
 export const ApiRequest = {
     GetInvestmentOptions : async() =>{
         const response = await fetch(ApiURL+"/Investment/GetInvestmentOptions");
         return response.json();
     },
     CalucateProjectedROI:  async(params) =>{
+        const investments = {
+            investment : params
+        };
         const options = {
             method: 'Post',
             headers: {
                'Content-Type': 'application/json'            
             },
-            body:  JSON.stringify(params)
+            body:  JSON.stringify(investments)
            }
            const response = await fetch(ApiURL+"/Investment/CalucateProjectedROI",options);
            return response.json();
